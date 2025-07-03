1e6aecae2980d48a41d20ccec05cf5841f43ef90
Uday Singh Rana <122337849+UdaySinghRana@users.noreply.github.com>
2025-07-03 11:36:32 +0530

    Add files via upload

--- first.java
+++ first.java
@@ -1,21 +1,8 @@
 import java.util.*;
 public class first {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        float floatValue1 = 2.0f;
-        float floatValue2 = 4.0f;
-        float floatResult = floatValue1+floatValue2;
-
-        System.out.println("Float: "+floatResult);
-
-
-        double doubleValue1 = 2.0;
-        double doubleValue2 = 4.0;
-        double doubleResult = doubleValue1 + doubleValue2;
-
-        System.out.println("Double: "+doubleResult);
-
-
+        System.out.println("Hello World");
+        System.out.println("Ram_Rahim");
 
 
 
